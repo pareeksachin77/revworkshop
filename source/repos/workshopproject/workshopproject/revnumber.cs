4193d9de8a96068ea0aa2c7439f09bcf05515e3c
pareeksachin77 <pareeksachin77@gmail.com>
2022-10-31 11:22:26 +0530

    workshopproject

---
+++ source/repos/workshopproject/workshopproject/revnumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshopproject
+{
+    internal class revnumber
+    {
+        public static void rev()
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int reversenumber = 0;
+            int temp = 0;
+
+            while(num > 0)
+            {
+                temp = num % 10;
+                num =num / 10;
+                reversenumber = reversenumber * 10 + temp;
+            }
+
+            Console.WriteLine("reverse number is" + reversenumber);
+        }
+    }
+}
